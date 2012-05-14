0c4a6cf2ad5ca62079a86aab01721a5440a6dd2b
Christopher J. Madsen <perl@cjmweb.net>
2012-05-14 20:36:10 +0000

    Import of CJM/Media-LibMTP-API-0.02 from CPAN.
    gitpan-cpan-distribution: Media-LibMTP-API
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         CJM/Media-LibMTP-API-0.02.tar.gz
    gitpan-cpan-author:       CJM
    gitpan-cpan-maturity:     released

--- lib/Media/LibMTP/API/Filetypes.pm
+++ lib/Media/LibMTP/API/Filetypes.pm
@@ -22,7 +22,7 @@ use strict;
 use warnings;
 
 our $VERSION = '0.01';
-# This file is part of Media-LibMTP-API 0.01 (May 12, 2012)
+# This file is part of Media-LibMTP-API 0.02 (May 14, 2012)
 
 use Exporter 5.57 'import';     # exported import method
 our @EXPORT_OK = qw(filetype filetype_from_path);
@@ -112,8 +112,8 @@ Media::LibMTP::API::Filetypes - Map extensions & MIME types to libmtp filetypes
 =head1 VERSION
 
 This document describes version 0.01 of
-Media::LibMTP::API::Filetypes, released May 12, 2012
-as part of Media-LibMTP-API version 0.01.
+Media::LibMTP::API::Filetypes, released May 14, 2012
+as part of Media-LibMTP-API version 0.02.
 
 =head1 SYNOPSIS
 
